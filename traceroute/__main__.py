2d47329b1e74fd00a0236618ee174079d25f3bbd
Nico Rittinghaus <nico.rittinghaus@hhu.de>
2023-04-20 13:14:33 +0200

    continued CLI improvements

--- traceroute/__main__.py
+++ traceroute/__main__.py
@@ -1,59 +1,76 @@
 import argparse
-from .traceroute import trace_udp
+from traceroute import trace_udp
 
 
 def main() -> int:
     host = None
+    epilog = """See also RFC2151 section 3.4 for a quick
+                read-up on traceroute."""
     parser = argparse.ArgumentParser(
                     prog='traceroute',
-                    description="""Provides a mostly *NIX traceroute 
-                    compliant -- but very basic -- tracert/traceroute 
+                    description="""Provides a mostly *NIX traceroute
+                    compliant -- but very basic -- tracert/traceroute
                     IPv4 alternative built on scapy.""",
-                    epilog='See also RFC2151.')
+                    epilog=epilog)
     parser.add_argument('remote_host', metavar='host', \
         help="""IP address or host name of the remote host
                 which is the destination of the route.""")
-    parser.add_argument('packet_length', metavar='plength', \
-        required=False, \
+    parser.add_argument('packet_length', metavar='packet_length', \
+        nargs='?', default=2, \
         help="""size of the UDP packet to be sent.""")
-    parser.add_argument('-m', '--maxttl', '--max-hops', default=30, \
+    parser.add_argument('-m', '--maxttl', '--max-hops', \
+                        default=30, \
         help="""maximum allowable TTL value, measured as
-                the number of hops allowed before the 
-                program terminates. 
+                the number of hops allowed before the
+                program terminates.
                 (default = 30)""")
-    parser.add_argument('-f', '--minttl', '--first', default=1, \
-        help="""minimum TTL value, measured as the number 
-                of hops at which to start. 
+    parser.add_argument('-f', '--minttl', '--first', \
+                        default=1, \
+        help="""minimum TTL value, measured as the number
+                of hops at which to start.
                 (default = 1)""")
-    parser.add_argument('-q', '--fleetsize', default=3, \
-        help="""number of UDP packets that will be sent 
-                with each time-to-live setting. 
+    parser.add_argument('-q', '--fleetsize', \
+                        default=3, \
+        help="""number of UDP packets that will be sent
+                with each time-to-live setting.
                 (default = 3)""")
-    parser.add_argument('-w', '--timeout', default=5, \
+    parser.add_argument('-w', '--timeout', \
+                        default=5, \
         help="""amount of time, in seconds, to wait for
-                an answer from a particular router before 
-                giving up. 
-                #TODO: MAX, HERE, NEAR not supported yet. 
+                an answer from a particular router before
+                giving up.
+                #TODO: MAX, HERE, NEAR not supported yet.
                 (default = 5)""")
-    parser.add_argument('-p', '-P', '--port', default=33434, \
-        help="""destination port (invalid) at the remote 
-                host. 
+    parser.add_argument('-p', '-P', '--port', \
+                        default=33434, \
+        help="""destination port (invalid) at the remote
+                host.
                 (default = 33434)""")
-    parser.add_argument('-s', '--source', default=None, \
+    parser.add_argument('-s', '--source', \
+                        default=None, \
         help="""source address of outgoing packets.
                 (default is address of adapter used)""")
-    parser.add_argument('-M', '--module', default='UDP',
-                        choices=['UDP'],
-        help="""module (or method) for traceroute 
+    parser.add_argument('-M', '--module', \
+                        default='UDP', choices=['UDP'], \
+        help="""module (or method) for traceroute
                 operations.
                 (default = UDP)""")
     args = parser.parse_args()
-    trace_udp(args.remote_host,    # TODO: alle args übernehmen
-          min_ttl=int(args.minttl),
-          max_ttl=int(args.maxttl), 
-          num_per_fleet=int(args.fleetsize), 
-          timeout=int(args.timeout),
-          port=int(args.port))
-    
+
+    match args.module:
+        case 'UDP':
+            trace_udp(args.remote_host,
+                udp_length=int(args.packet_length),
+                min_ttl=int(args.minttl),
+                max_ttl=int(args.maxttl),
+                num_per_fleet=int(args.fleetsize),
+                timeout=int(args.timeout),
+                port=int(args.port),
+                source=str(args.source))
+        case 'ICMP':
+            print("ICMP mode is not supported yet.") # TODO
+        case _:
+            print('Module ' + args.module + ' is not supported.')
+
 
 main()
--- traceroute/traceroute.py
+++ traceroute/traceroute.py
@@ -3,23 +3,35 @@ from abc import ABC, abstractmethod
 from scapy.all import IP, UDP, sr1
 
 
-def trace_udp(host:str, 
-          udp_length=2,
-          max_ttl:int=30,
-          num_per_fleet:int=3,
-          timeout:int=5,
-          port:int=33434):
+def trace_udp(host:str,
+              udp_length=2,
+              min_ttl:int=1,
+              max_ttl:int=30,
+              num_per_fleet:int=3,
+              timeout:int=5,
+              port:int=33434,
+              source:str=None):
     data:str = get_junk(udp_length)
     responses = list()
-    for ttl in range(1, max_ttl):
+    for ttl in range(min_ttl, max_ttl+1):
         for pckno in range(num_per_fleet):
-            packet = IP(dst='host', ttl=ttl) / \
-                     UDP(dport=port) / \
-                     data
+            packet = make_packet(host, source, ttl, port, data)
             response = sr1(packet, timeout=timeout)
             responses.append(response)
-            
-            
+            #print(response) # TODO: interpret response
+
+
+def make_packet(host:str, source:str, ttl:int, port:int, data):
+    if source != None:
+        packet = IP(dst=host, src=source, ttl=ttl) / \
+                 UDP(dport=port) / \
+                 data
+    else:
+        packet = IP(dst=host, ttl=ttl) / \
+                 UDP(dport=port) / \
+                 data
+    return packet
+
 def get_junk(length=2):
     ret = "42" * length
     if length%2 != 0:
